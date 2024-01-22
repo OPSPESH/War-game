c0284f42639b6e53cefcff5bad1f0f23770dd3ae
king-squid <150670865+king-squid@users.noreply.github.com>
2024-01-22 11:29:19 +0000

    Add files via upload
    this is my attempt at the code for the war game it needs work

---
+++ meue for war game.py
@@ -0,0 +1,25 @@
+import random
+def dice(dice1):# defines the word dice
+    number = random.randint(1,6)#code for dice(RANOM NUMBER GENORATOR)
+    print(number)#prints the result of the dice
+    if dice1 == 'home':
+        return
+    
+def cards():
+    card1 = input("search card here ::")
+    if card1 == ("panther"):
+        print("panther card info")
+    
+        
+
+while True:
+    print("                                 6 sided dice = 1 ")#gives option
+    print("                                 combat cards = 2") #gap for looks
+    dice1 = input("                         select an option")
+    if dice1 == ("1"):
+        dice(dice1)
+    elif dice1 == ("2"):
+     cards()
+    
+    
+ 
