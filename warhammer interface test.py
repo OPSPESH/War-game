5aad940011826987cbf5bfdf8f82eef12f64b1dd
Oliver Parr <139624027+OPSPESH@users.noreply.github.com>
2024-01-30 10:38:26 +0000

    Add files via upload

---
+++ warhammer interface test.py
@@ -0,0 +1,68 @@
+import random, os 
+
+def dice_roller():
+    while True:
+        num = input('Enter a number from [1-20] or "home" to exit')
+        if num in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']:
+            res_list = [random.randint(1,6) for i in range(int(num))]
+        elif num == 'home':
+            return 
+        print(res_list)
+
+def cards():
+    while True:
+        print('What would you like to accsess or "home" to close: ')
+        print('1) Troops')
+        print('2) Tanks')
+        print('3) Planes')
+        print('4) Misc')
+        inp = input()
+        if inp == '1':
+            print('one')
+        elif inp == '2':
+            print(os.listdir("cards/tanks"))
+            inp = input('Select a card')
+            if inp == 'churchhill':
+                card = open('cards/tanks/churchhill.txt')
+                print('')
+                print(card.read())
+                print('')
+                card.close()
+            elif inp == 'stug3' or 'stug-3':
+                card = open('cards/tanks/stug-3.txt')
+                print('')
+                print(card.read())
+                print('')
+                card.close()
+        elif inp == '3':
+            print('three')
+        elif inp == '4':
+            print('four')
+        elif inp == 'home':
+            return
+
+def rules():
+    while True:
+        print('What rule book would you like to access: ')
+        print('1) semi-action')
+        inp = input()
+        if inp == '1':
+            file = open('semi-action-rules.txt')
+            print(file.read())
+            print('')
+            
+while True:
+    print('Welcome to the Bolt Action TUI')
+    print('What would you like to do')
+    print('1) Dice roller')
+    print('2) Cards')
+    print('3) Rule book')
+    prog = input('What would you like to accsess or "home" to exit: ')
+    if prog == '1':
+        dice_roller()
+    elif prog == '2':
+        cards()
+    elif prog == '3':
+        rules()
+    elif prog == 'home':
+        exit()
