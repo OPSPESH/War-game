5edd45bd8de3ad7600c7de333b3680c1a7dd1a11
king-squid <150670865+king-squid@users.noreply.github.com>
2024-03-14 09:03:33 +0000

    Add files via upload

--- meue for war game.py
+++ meue for war game.py
@@ -1,25 +1,24 @@
 import random
+
 def dice(dice1):# defines the word dice
     number = random.randint(1,6)#code for dice(RANOM NUMBER GENORATOR)
     print(number)#prints the result of the dice
-    if dice1 == 'home':
-        return
+
     
-def cards():
-    card1 = input("search card here ::")
-    if card1 == ("panther"):
-        f = open("card basic all infantry*,txt", "r")
-        print(f.read())  
     
         
 
 while True:
     print("                                 6 sided dice = 1 ")#gives option
     print("                                 combat cards = 2") #gap for looks
-    dice1 = input("                         select an option")
+    dice1 = input("                         select an option   ")
     if dice1 == ("1"):
         dice(dice1)
     elif dice1 == ("2"):
-     cards()
+         dice1 = input("search card here ::")
+    if dice1 == ("infantry"):
+        f = open("H:/card game/card basic all infantry.txt")
+        print(f.read())
+       
   
  
