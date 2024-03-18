4c00d0f063b3cc2510fad26986b5ee002612b02b
Oliver Parr <139624027+OPSPESH@users.noreply.github.com>
2024-03-18 12:20:16 +0000

    Update meue for war game.py

--- meue for war game.py
+++ meue for war game.py
@@ -4,14 +4,10 @@ def dice(dice1):# defines the word dice
     number = random.randint(1,6)#code for dice(RANOM NUMBER GENORATOR)
     print(number)#prints the result of the dice
 
-    
-    
-        
-
 while True:
-    print("                                 6 sided dice = 1 ")#gives option
-    print("                                 combat cards = 2") #gap for looks
-    dice1 = input("                         select an option   ")
+    print("6 sided dice = 1 ")#gives option
+    print("combat cards = 2") #gap for looks
+    dice1 = input("select an option   ")
     if dice1 == ("1"):
         dice(dice1)
     elif dice1 == ("2"):
